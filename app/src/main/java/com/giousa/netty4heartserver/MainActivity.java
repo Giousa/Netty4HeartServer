550c7f77ca2e6a894efed7fbeafdbd96c147883f
Giousa <65489469@qq.com>
2017-02-09 10:42:15 +0800

    first commit

---
+++ app/src/main/java/com/giousa/netty4heartserver/MainActivity.java
@@ -0,0 +1,71 @@
+package com.giousa.netty4heartserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.giousa.netty4heartserver.handler.ServerHandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        Button start = (Button) findViewById(R.id.btn_server);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                serverStart();
+            }
+        });
+    }
+
+    private void serverStart() {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workGroup = new NioEventLoopGroup(4);
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap
+                    .group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline p = socketChannel.pipeline();
+                            p.addLast(new IdleStateHandler(10, 0, 0));
+                            p.addLast(new LengthFieldBasedFrameDecoder(1024, 0, 4, -4, 0));
+                            p.addLast(new ServerHandler());
+                        }
+                    });
+
+            Channel ch = bootstrap.bind(12345).sync().channel();
+
+            System.out.println("------Server Start------");
+
+            ch.closeFuture().sync();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ app/src/main/java/com/giousa/netty4heartserver/handler/ServerHandler.java
@@ -0,0 +1,43 @@
+package com.giousa.netty4heartserver.handler;
+
+import com.giousa.netty4heartserver.common.CustomHeartbeatHandler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Description:
+ * Author:Giousa
+ * Date:2017/2/9
+ * Email:65489469@qq.com
+ */
+public class ServerHandler extends CustomHeartbeatHandler {
+
+    public ServerHandler() {
+        super("server");
+    }
+
+    @Override
+    protected void handleData(ChannelHandlerContext channelHandlerContext, ByteBuf buf) {
+        byte[] data = new byte[buf.readableBytes() - 5];
+        ByteBuf responseBuf = Unpooled.copiedBuffer(buf);
+        buf.skipBytes(5);
+        buf.readBytes(data);
+        String content = new String(data);
+        System.out.println(name + " get content: " + content);
+        channelHandlerContext.write(responseBuf);
+    }
+
+    @Override
+    protected void handleReaderIdle(ChannelHandlerContext ctx) {
+        super.handleReaderIdle(ctx);
+        System.err.println("---client " + ctx.channel().remoteAddress().toString() + " reader timeout, close it---");
+        ctx.close();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println(name+" exception"+cause.toString());
+    }
+}
